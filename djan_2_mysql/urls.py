62886526cae851032efec611d12c044ffdbb8d51
dosrka <977982472@qq.com>
2017-10-30 11:21:55 +0800

    begin post

--- djan_2_mysql/urls.py
+++ djan_2_mysql/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from web_app.views import index
+from web_app.views import index, del_content
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index)
+    url(r'^$', index),
+    url(r'^del+', del_content)
 ]
--- web_app/views.py
+++ web_app/views.py
@@ -1,34 +1,18 @@
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core import serializers
 from web_app import models
 import json
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-# def index(request):
-#     city_list = list()
-#     city_list_json = serializers.serialize("json", models.City.objects.all())
-#     # 取出的json数据为str格式
-#     city_list_dic = json.loads(city_list_json)
-#     # 转换后的list，里面都是字典
-#     for i in city_list_dic:
-#         city_list.append(i)
-#     print(city_list)
-#     return render(request, 'index.html', {'city_list': city_list})
-
-
 def index(request):
     # 每页显示的数据条数
-    limit = 50
-    city_list = list()
+    limit = 70
     city_list_json = serializers.serialize("json", models.CityCopy1.objects.all())
     # 取出的json数据为str格式
-    city_list_dic = json.loads(city_list_json)
+    city_list = json.loads(city_list_json)
     # 转换后的list，里面都是字典
-    for i in city_list_dic:
-        city_list.append(i)
-    print(city_list)
     paginator = Paginator(city_list, limit)
 
     page = request.GET.get('page')
@@ -40,3 +24,9 @@ def index(request):
         city_list = paginator.page(paginator.num_pages)
 
     return render(request, 'index.html', {'city_list': city_list})
+
+
+def del_content(request):
+    nid = request.GET.get("nid")
+    models.CityCopy1.objects.filter(id=nid).delete()
+    return redirect(index)
